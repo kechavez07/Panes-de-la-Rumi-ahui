18478250ac5b4ca954b845351a90f7604adcda44
y6u7i8o9p0Y&U/I(O)P= <alejodiego536@gmail.com>
2025-02-17 20:25:45 -0500

    slidebar corregido

--- panes-de-la-ruminahui-v2/src/components/sidebar/Sidebar.jsx
+++ panes-de-la-ruminahui-v2/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './sidebar.css';
 
-const Sidebar = ({ isOpen, toggleSidebar }) => {
+const Sidebar = ({ isOpen }) => {
   return (
     <>
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <button onClick={toggleSidebar} className="sidebar-toggle">
-        {isOpen ? 'Cerrar' : 'Abrir'} Sidebar
-      </button>
       <ul>
         <li>Inicio</li>
         <li>Acerca de</li>
--- panes-de-la-ruminahui-v2/src/pages/Dashboart.jsx
+++ panes-de-la-ruminahui-v2/src/pages/Dashboart.jsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/sidebar/Sidebar';
 import './dashboart.css';
 
 export default function Dashboard() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     console.log('Dashboard - isSidebarOpen:', isSidebarOpen); // Verifica si el estado se actualiza
   
     const toggleSidebar = () => {
@@ -12,8 +12,11 @@ export default function Dashboard() {
   
     return (
       <div>
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <Sidebar isOpen={isSidebarOpen} />
         <div className={`dashboard ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+          <button onClick={toggleSidebar} className="sidebar-toggle">
+            {isSidebarOpen ? 'Cerrar' : 'Abrir'} Sidebar
+          </button>
           <h1>Dashboard</h1>
           <p>Welcome to the Dashboard!</p>
           <p>Here you can see all the information about your account.</p>
